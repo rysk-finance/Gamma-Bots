b22c4ddd8f783ebc61543500e0c285a1d8d86511
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-05-03 14:32:02 +0100

    update testnet addresses

--- bots/openzeppelin-defender/testnet/chainlink-pricer-bot.js
+++ bots/openzeppelin-defender/testnet/chainlink-pricer-bot.js
@@ -1,9 +1,6 @@
 require('dotenv').config()
 
-const {
-	DefenderRelaySigner,
-	DefenderRelayProvider
-} = require('defender-relay-client/lib/ethers')
+const { DefenderRelaySigner, DefenderRelayProvider } = require('defender-relay-client/lib/ethers')
 const chainlinkPricerLogic = require('../../core-logic/chainlink-pricer')
 
 // Entrypoint for the Autotask
@@ -11,10 +8,9 @@ exports.handler = async function (credentials) {
 	// config
 	const relayerAddress = '0xa7d48256291bcd02656b05e7d38bd5cb617edb29' // Relayer address
 	const addressbookAddress = '0xd6e67bF0b1Cdb34C37f31A2652812CB30746a94A' // AddressBook module
-	const pricerAddress = '0x7140E3089ec5d5EF0187BdEd69a5b0333d608fDF' // WETH pricer
-	const pricerAsset = '0x53320bE2A35649E9B2a0f244f9E9474929d3B699' // WETH address
-	const chainlinkAggregatorAddress =
-		'0x62CAe0FA2da220f43a51F86Db2EDb36DcA9A5A08' // Chainlink price feed
+	const pricerAddress = '0x7f410Ea16AdeCBa3194D3C41bCB732FA1eA37493' // WETH pricer
+	const pricerAsset = '0x3b3a1dE07439eeb04492Fa64A889eE25A130CDd3' // WETH address
+	const chainlinkAggregatorAddress = '0x62CAe0FA2da220f43a51F86Db2EDb36DcA9A5A08' // Chainlink price feed
 
 	// Initialize default provider and defender relayer signer
 	const provider = new DefenderRelayProvider(credentials)
@@ -33,10 +29,8 @@ exports.handler = async function (credentials) {
 
 // To run locally (this code will not be executed in Autotasks)
 if (require.main === module) {
-	const {
-		PRICER_BOT_TESTNET_API_KEY: apiKey,
-		PRICER_BOT_TESTNET_API_SECRET: apiSecret
-	} = process.env
+	const { PRICER_BOT_TESTNET_API_KEY: apiKey, PRICER_BOT_TESTNET_API_SECRET: apiSecret } =
+		process.env
 	exports
 		.handler({
 			apiKey,
