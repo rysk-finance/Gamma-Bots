09fd28f3dc23365185965999629494e1fd378fe9
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-07-18 22:53:15 +0100

    lint sabr updater

--- bots/core-logic/sabr-updater.js
+++ bots/core-logic/sabr-updater.js
@@ -4,43 +4,32 @@ const { ethers } = require("ethers")
 const exchangeAbi = require("../abi/OptionExchange.json")
 const volFeedAbi = require("../abi/VolatilityFeed.json")
 
-const sabrUpdaterLogic = async (
-	signer, volFeedAddress, exchangeAddress, queryParameters
-) => {
-	const volFeed = new ethers.Contract(
-		volFeedAddress,
-		volFeedAbi,
-		signer
-	)
-  const exchange = new ethers.Contract(
-		exchangeAddress,
-		exchangeAbi,
-		signer
-	)
-	
+const sabrUpdaterLogic = async (signer, volFeedAddress, exchangeAddress, queryParameters) => {
+	const volFeed = new ethers.Contract(volFeedAddress, volFeedAbi, signer)
+	const exchange = new ethers.Contract(exchangeAddress, exchangeAbi, signer)
 
-  sabr_params = queryParameters.sabr_params;
-  sabr = JSON.parse(sabr_params);
-  console.log(sabr);
+	sabr_params = queryParameters.sabr_params
+	sabr = JSON.parse(sabr_params)
+	console.log(sabr)
 
-  const isPausedBefore = await exchange.paused();
-  if (!isPausedBefore) {
-  	let tx = await exchange.pause({ gasLimit: '10000000' });
-    tx.wait()
-    console.log("exchange paused")
-  }
-  for (let i=0; i < sabr.length; i++) {
-    const ts = sabr[i].shift()
-    tx = await volFeed.setSabrParameters(sabr[i], ts, { gasLimit: '10000000' })
-    console.log('Tx hash: ', tx.hash)
-  }
-  // if the system was not paused prior, then unpause
-  // otherwise leave paused
-  if (!isPausedBefore) {
-      let tx = await exchange.unpause({ gasLimit: '10000000' });
-      tx.wait()
-      console.log("exchange unpaused")
-    }
+	const isPausedBefore = await exchange.paused()
+	if (!isPausedBefore) {
+		let tx = await exchange.pause({ gasLimit: "10000000" })
+		tx.wait()
+		console.log("exchange paused")
+	}
+	for (let i = 0; i < sabr.length; i++) {
+		const ts = sabr[i].shift()
+		tx = await volFeed.setSabrParameters(sabr[i], ts, { gasLimit: "10000000" })
+		console.log("Tx hash: ", tx.hash)
+	}
+	// if the system was not paused prior, then unpause
+	// otherwise leave paused
+	if (!isPausedBefore) {
+		let tx = await exchange.unpause({ gasLimit: "10000000" })
+		tx.wait()
+		console.log("exchange unpaused")
+	}
 }
 
 module.exports = sabrUpdaterLogic
--- bots/tenderly/mainnet/sabr-updater.js
+++ bots/tenderly/mainnet/sabr-updater.js
@@ -7,7 +7,6 @@ exports.handler = async function () {
 	// config
 	const volFeedAddress = "0xF204B60A98B3be05914AeC46bcEd2476D13a0225"
 	const exchangeAddress = "0xC117bf3103bd09552F9a721F0B8Bce9843aaE1fa"
-	const relayerAddress = "0xd5730eDE6Ed7f9051E9E55eB4B507b4F48C46111" // Relayer address
 
 	// Initialize default provider signer
 	const provider = new ethers.providers.JsonRpcProvider(process.env.ALCHEMY_RPC_ENDPOINT)
