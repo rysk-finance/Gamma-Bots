3a869779609c26afa8629c810330624bff74535c
Josh <55187183+JoshDAO@users.noreply.github.com>
2023-10-05 15:02:39 +0100

    undo commented code

--- bots/core-logic/range-order-fulfill.js
+++ bots/core-logic/range-order-fulfill.js
@@ -9,16 +9,17 @@ const rangeOrderFulfillLogic = async (signer, reactorAddress) => {
 	const currentPosition = await reactor.currentPosition()
 
 	console.log(currentPosition)
-	// if (
-	// 	currentPosition.activeLowerTick == 0 ||
-	// 	currentPosition.activeUppertick == 0 ||
-	// 	currentPosition.activeLowerTick == currentPosition.activeUppertick
-	// ) {
-	// 	console.log("nothing to do")
-	// 	return
-	// }
+	if (
+		currentPosition.activeLowerTick == 0 ||
+		currentPosition.activeUppertick == 0 ||
+		currentPosition.activeLowerTick == currentPosition.activeUppertick
+	) {
+		console.log("nothing to do")
+		return
+	}
 
 	const poolPrice = (await reactor.getPoolPrice()).price
+	console.log({ poolPrice })
 
 	if (
 		(currentPosition.activeRangeAboveTick && poolPrice > currentPosition.activeUpperTick) ||
@@ -27,8 +28,6 @@ const rangeOrderFulfillLogic = async (signer, reactorAddress) => {
 		console.log("fulfilling range order")
 		await reactor.fulfillActiveRangeOrder()
 	}
-
-	console.log({ poolPrice })
 }
 
 module.exports = rangeOrderFulfillLogic
